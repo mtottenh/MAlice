5ab3974ab4743927ef2026b367c11cd936339532
Max Tottenham <mt3410@imperial.ac.uk>
2012-11-15 13:30:23 +0000

    tree_print.cpp: minor edits, just have to implement print() and getchildren() for each node[3~

--- tree_print.cpp
+++ tree_print.cpp
@@ -1,12 +1,17 @@
 #include "node.h"
 #include <queue>
+
+
 int tree_print(Node *root) {
 	std::queue<Node *> printList;
 	printList.push(root);
 	while(printList.empty()) {
 		Node *temp = printList.pop();
 		temp->print();
-		printlist.push(temp->children[1..n]);
+		std::queue<Node *> children = temp->getchildren();
+		while (children.empty()) {
+			printlist.push(children.pop());
+		}
 	}
 	
 }
