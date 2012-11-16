8c7ad20fd7ae56633d3fbf8cdde8759234b518f4
Max Tottenham <mt3410@imperial.ac.uk>
2012-11-16 13:09:22 +0000

    Working on removing circular adding of nodes from the tree, I believe it is to do with the declaration list clause - note this revision does not compile

--- TreePrinter.cpp
+++ TreePrinter.cpp
@@ -8,23 +8,41 @@ treePrinter::treePrinter(Node *tree) {
 }
 int treePrinter::print() {
 	std::queue<const Node *> printList;
+	std::queue<int> level;
+	int print_level = 1;
+	level.push(print_level);
 	printList.push(root);
 	while(!printList.empty()) {
 		const Node *temp = printList.front();
 		printList.pop();
 		if (temp != NULL) {
-			temp->print(); 
+			int current_level = level.front();
+			level.pop();
+			if(print_level< current_level) {
+				cout <<endl;	
+				print_level = current_level;
+				cout << "Level : " << print_level << endl;
+			}
+			cout << "Type: ";
+			temp->print();
+			cout << "\t"; 
 			const std::vector<Node *> children = temp->getChildren();
-			cout << "Current Node has: " << children.size() << " children" << endl;
-			for (int i = 0; i< children.size(); i++){
-				if(children[i] != temp) {
+/*			cout << "\tCurrent Node has: " << children.size() << " children" ; */
+			
+			for (unsigned int i = 0; i< children.size(); i++){
+				if((children[i] != temp) && (children[i] != NULL)){
 					printList.push(children[i]);
+					level.push(current_level + 1);
+				} else {
+					cout << "\tPROBLEM NODE AT i = " << i;
 				}
-		}
+			}
 	
-}
+		}
 	}
-};
+	cout << endl;
+	return 0;
+}
 
 /* used for some basic testing 
 int main () {
