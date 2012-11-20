35ce067a3ed042a55f09437e2fb99a1ec1358518
Max Tottenham <mt3410@imperial.ac.uk>
2012-11-20 17:37:43 +0000

    NFunctionDeclaration.cpp: Added New constructor implementation, at the moment we are storing the 'identifier string' in a variable called name, it may be better to create a new field of the NFunctionDeclaration called 'idstring' and store it in there

--- Node/NFunctionDeclaration.hpp
+++ Node/NFunctionDeclaration.hpp
@@ -2,13 +2,16 @@
 #define _NFUNCTIONDECLARATION_HPP_
 
 #include "Node.hpp"
-
+#include "NIdentifier.hpp"
                                                                                  
 class NFunctionDeclaration : public Node {                          
                                                                                  
 public:                                                                          
         NFunctionDeclaration();                           
-        NFunctionDeclaration(Node*);
+	NFunctionDeclaration(NIdentifier*, Node*, Node* , int);
+	NFunctionDeclaration(NIdentifier*, Node*, int);
+	NFunctionDeclaration(NIdentifier*, Node*, Node*);
+	NFunctionDeclaration(NIdentifier*, Node*);
 	int print() const;
 };    
 
