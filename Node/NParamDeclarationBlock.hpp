0825719d6b2d3fad85064e79f180956d28316a33
Mitchell Allison <mka211@doc.ic.ac.uk>
2012-11-21 15:17:26 +0000

    Stripped quotes from the StringLit AST node

---
+++ Node/NParamDeclarationBlock.hpp
@@ -0,0 +1,13 @@
+#ifndef _NPARAMDECLARATIONBLOCK_H_
+#define _NPARAMDECLARATIONBLOCK_H_
+
+#include "Node.hpp"
+
+class NParamDeclarationBlock : public Node
+{
+public:
+	NParamDeclarationBlock();
+	NParamDeclarationBlock(Node*);
+};
+
+#endif
