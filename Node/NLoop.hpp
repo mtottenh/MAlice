398929aaaaeec78094a230e66bf6041180061772
Sean Allan <sra11@doc.ic.ac.uk>
2012-11-22 18:54:09 +0000

    NLoop type resolution and check function

--- Node/NLoop.hpp
+++ Node/NLoop.hpp
@@ -3,6 +3,11 @@
 
 #include "Node.hpp"
 class NLoop : public Node {                                                
+/*
+ * The loop inherits type resolution from the base Node (does not have a type).
+ * The check function also inherits from Node - the loop is valid if its child
+ * statements are valid.
+ */
 public:
 	NLoop(Node* predicate, Node* statementList); 
 }; 
