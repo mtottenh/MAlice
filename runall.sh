7214b4f88d1366cfac05950ff2d8e7f90f05f7b9
Mitchell Allison <mka211@doc.ic.ac.uk>
2012-11-23 12:52:21 +0000

    Changed test file to not need a redirect following the parser command

--- runall.sh
+++ runall.sh
@@ -1,11 +1,11 @@
 for f in ../malice_examples/archaeology/*.alice
 do
 	echo "***** $f *****"
-	./parser < "$f"	
+	./parser "$f"	
 done
 
 for f in ../malice_examples/valid/*.alice
 do
 	echo "***** $f *****"
-	./parser < "$f"
+	./parser "$f"
 done
