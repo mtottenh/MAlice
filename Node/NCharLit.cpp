ea5b06a8cbd74c4c36990f6801b1aba16562b636
Sean Allan <sra11@doc.ic.ac.uk>
2012-11-27 15:22:35 +0000

    Refactored NCharLit

--- Node/NCharLit.cpp
+++ Node/NCharLit.cpp
@@ -1,23 +1,36 @@
 #include "NCharLit.hpp"
 #include "TypeDefs.hpp"
 
+/* Constructors. */
 NCharLit::NCharLit() {
-	name = "<Character Literal>";
-	type = resolveType();
-	nodeType = CHARLIT;
+	this->name = "<Character Literal>";
+	this->type = resolveType();
+	this->nodeType = CHARLIT;
 }
 
 NCharLit::NCharLit(char *c) {
+	/* Set the character field to the char literal, minus the ' symbol. */
 	this->character = *++c;
-	name = character;
-	type = resolveType();
-	nodeType = CHARLIT;
+	this->name = character;
+	this->type = resolveType();
+	this->nodeType = CHARLIT;
 }
 
+/* Public methods. */
+
+int NCharLit::check() {
+	/* 
+	 * This is a node representing a base type, and the parser ensures it is
+	 * always valid.
+	 */
+	return 1;
+}
+
+/* Private methods. */
+
 int NCharLit::resolveType() {
+	/* A character literal is always of type character! (letter in MAlice). */
 	return TCHAR;
 }
 
-int NCharLit::check() {
-	return 1;
-}
+
--- Node/NCharLit.hpp
+++ Node/NCharLit.hpp
@@ -5,11 +5,22 @@
 
 class NCharLit : public Node { 
 private:
+	/* Field that stores the character literal of the node. */
 	char character;
+
+	/* Resolves the type of the node and returns it. */
 	virtual int resolveType();                                           
 public:
+	/* Constructor passed no character literal value. */
 	NCharLit();
+
+	/* Constructor taking a character literal value. */
 	NCharLit(char *);
+
+	/*
+	 * Checks if the node is semantically valid. Returns 1 if it is valid,
+	 * 0 otherwise.
+	 */
 	virtual int check();
 };  
 
