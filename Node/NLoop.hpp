614b3a8d3eb5228b21c205e928c439ee1a6ad86b
Sean Allan <sra11@doc.ic.ac.uk>
2012-11-27 16:46:56 +0000

    Refactored NLoop

--- Node/NLoop.hpp
+++ Node/NLoop.hpp
@@ -1,3 +1,5 @@
+/* Node describing a loop in the MAlice language. */
+
 #ifndef _NLOOP_HPP
 #define _NLOOP_HPP
 
@@ -10,7 +12,16 @@ class NLoop : public Node {
  * returns a Boolean.
  */
 public:
-	NLoop(Node* predicate, Node* statementList);
+	/*
+	 * Constructor that takes a predicate node and a statement list node for the
+	 * loop.
+	 */
+	NLoop(Node*, Node*);
+
+	/*                                                                           
+ 	 * Checks that the node is semantically valid. Returns 1 if this is the      
+ 	 * case, 0 otherwise.                                                        
+ 	 */
 	virtual int check(); 
 }; 
 
