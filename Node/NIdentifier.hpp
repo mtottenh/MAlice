f94c5e1bab049d4884766ae25108ed5d038d4892
Sean Allan <sra11@doc.ic.ac.uk>
2012-11-27 16:10:55 +0000

    Refactored NIdentifier

--- Node/NIdentifier.hpp
+++ Node/NIdentifier.hpp
@@ -1,3 +1,5 @@
+/* Node representing an identifier in the MAlice language. */
+
 #ifndef _NIDENTIFIER_HPP_
 #define _NIDENTIFIER_HPP_
 
@@ -5,13 +7,26 @@
 #include <string>
 
 class NIdentifier : public Node {
-        public:
+public:
+		/* Empty constructor. */
        	NIdentifier();
-		string getID();
+		
+		/* Sets the identifiers value to an id given by a string or char* */
 		NIdentifier(string);
-	      	NIdentifier(char*);                                
-		int check(); 
-		int getType() ;
+	    NIdentifier(char*);
+
+		/* Return the data type of the node. */
+		int getType();
+
+		/* Returns the identifier of the... identifier. */  
+		string getID();
+        
+		/*                                                                           
+ 		 * Checks that the node is semantically valid. Returns 1 if this is the      
+ 		 * case, 0 otherwise.                                                        
+ 		 */       
+		virtual int check(); 
+	
 };  
 
 #endif
