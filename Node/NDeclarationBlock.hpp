77bc8842a675c6caca9f1eff701aa981c6958608
Sean Allan <sra11@doc.ic.ac.uk>
2012-11-27 15:52:40 +0000

    Refactored NDeclarationBlock

--- Node/NDeclarationBlock.hpp
+++ Node/NDeclarationBlock.hpp
@@ -1,15 +1,24 @@
+/* Node representing a MAlice declaration block. */
+
 #ifndef _NDECLARATIONBLOCK_HPP_
 #define _NDECLARATIONBLOCK_HPP_
 
 #include "Node.hpp"
 class NDeclarationBlock : public Node {  
+private:
+	/* Check function for a root node. Behaves in the same way as check(). */
+	int checkRoot();
 public:
-	/*
-	 * Type resolution and checking are inherited from Node superclass.
-	 * Small addition to check for 'hatta' entry point!
-	 */	
+	/* Empty constructor, taking no child nodes. */
 	NDeclarationBlock();
+	
+	/* Constructor taking a child node. */
 	NDeclarationBlock(Node *child);
+
+	/*                                                                           
+	 * Checks that the node is semantically valid. Returns 1 if this is the      
+ 	 * case, 0 otherwise.                                                        
+	 */
 	virtual int check();
 };  
 
