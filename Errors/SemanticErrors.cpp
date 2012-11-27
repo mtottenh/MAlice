30212340def5c18373a1c869e0fee9409f899a8f
Sean Allan <sra11@doc.ic.ac.uk>
2012-11-27 13:11:27 +0000

    NStringLit now sets it's name/identifier to the literal string. Error messages now also mention that the identifier could potentially be a literal.

--- Errors/SemanticErrors.cpp
+++ Errors/SemanticErrors.cpp
@@ -10,14 +10,15 @@ void error_keyword(const string& keyword) {
 }
 
 void error_var_not_found(const string& varName) {
-	cerr << "ERROR: Identifier, variable or method '" << varName << 
+	cerr << "ERROR: Identifier, variable, literal or method '" << varName << 
 		"' not found in scope." << endl;
 }
 
 void error_type_mismatch(const string& varName, int t1, int t2) {
-	cerr << "ERROR: Type mismatch. Variable '" << varName << "' of type '"
-		<< typemap_get(t1) << "' does not match expected type '" << 
-		typemap_get(t2) << "'" << endl;
+	cerr << "ERROR: Type mismatch. Variable or literal '" << varName 
+		<< "' of type '" << typemap_get(t1) 
+		<< "' does not match expected type '" << typemap_get(t2) 
+		<< "'" << endl;
 }
 
 void error_type_mismatch(int op, int t1, int t2) {
--- Node/NStringLit.cpp
+++ Node/NStringLit.cpp
@@ -9,11 +9,13 @@ NStringLit::NStringLit(char* literal)
 	this->literal = temp.substr(1, temp.size() - 1);
 	nodeType = STRINGLIT;	
 	this->type = resolveType();
+	this->name = literal;
 
 }
 /* TODO check to see if we need to remove the " " from s*/
 NStringLit::NStringLit(string s) {
 	literal = s;
+	name = s;
 	type = resolveType();
 }
 int NStringLit::resolveType() {
