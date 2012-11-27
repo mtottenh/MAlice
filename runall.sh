0b2daef754ad8814cbb4c2fdfd5edd2a0ad02237
Max Tottenham <mtottenh@gmail.com>
2012-11-27 15:40:24 +0000

    Refators...

--- runall.sh
+++ runall.sh
@@ -1,17 +1,20 @@
 for f in ../malice_examples/archaeology/*.alice
 do
 	echo "***** $f *****" 1>&2
-	./parser "$f"	
+	./parser "$f"
+    echo "" 1>&2
 done
 
 for f in ../malice_examples/valid/*.alice
 do
 	echo "***** $f *****" 1>&2
 	./parser "$f"
+    echo "" 1>&2
 done
 
 for f in ../malice_examples/invalid/*.alice
 do
 	echo "***** $f *****" 1>&2
 	./parser "$f"
+    echo "" 1>&2
 done
