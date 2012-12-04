d9022abeb9ff6ae81cdcf740e5ad0adfa64d3ee9
Mitchell Allison <mka211@doc.ic.ac.uk>
2012-12-04 14:51:34 +0000

    Stopped herping, LabelMaker added to git repo

---
+++ CodeGeneration/LabelMaker.hpp
@@ -0,0 +1,24 @@
+#ifndef _LABELMAKER_H_
+#define _LABELMAKER_H_
+
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class LabelMaker
+{
+private:
+	int currentLabelNumber;
+	string* endLabel;
+	stack<string*> endCondLabelStack;
+public:
+	LabelMaker();
+	string getNewLabel();
+	string getEndCondLabel();
+	void resetEndCondLabel();
+	void pushEndCondLabel();
+	void popEndCondLabel();
+};
+
+#endif
--- CodeGeneration/x86Visitor.cpp
+++ CodeGeneration/x86Visitor.cpp
@@ -32,16 +32,32 @@ void x86Visitor::visit(NCodeBlock *node) {
 }
 
 void x86Visitor::visit(NConditional *node) {
+	string endLabel = this->labelMaker.getEndCondLabel();
+	// Visit the predicate to print out condition
+	node->getChild(0)->accept(this);
+	// If condition is false, jump to next condition
+	data << "jz " << this->labelMaker.getNewLabel() << endl;
+	// Otherwise, execute the statement list, first pushing
+	// the end condition label onto the stack
+	this->labelMaker.pushEndCondLabel();
+	node->getChild(1)->accept(this);
+	this->labelMaker.popEndCondLabel();
+	// Return to end of conditional
+	data << "jz " << endLabel << endl;
+	// Print out code for rest of cases
+	node->getChild(2)->accept(this);
+	//
+	// SHOULD ONLY BE DOING THIS THE FIRST TIME!
+	data << endLabel << ":" << endl;
+}
 
+void x86Visitor::visit(NEndIf *node) {
+	this->labelMaker.resetEndCondLabel();
 }
 
 /* NDeclarationBlock is our 'entry' point */
 void x86Visitor::visit(NDeclarationBlock *node) {
     cerr << "Declaration Block" << endl;
-}
-
-void x86Visitor::visit(NEndIf *node) {
-
 }
 
 void x86Visitor::visit(NFunctionDeclaration *node) {
